85b179aa4bc97185b717ac800cb3d77d0b63e25f
Gherks <nils-joakim.andersson@hotmail.com>
2021-10-02 11:30:23 +0200

    Add font awesome buttons to ingredient table

--- Receptacle/ClientApp/src/components/ingredientPage/IngredientsTable.tsx
+++ Receptacle/ClientApp/src/components/ingredientPage/IngredientsTable.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Ingredient from '../../dto/Ingredient';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons'
+
 export default function IngredientsTable(props: { ingredients: Ingredient[] }) {
     return (
         <>
@@ -12,6 +15,7 @@ export default function IngredientsTable(props: { ingredients: Ingredient[] }) {
                         <th>Carbohydrates</th>
                         <th>Protein</th>
                         <th>Calories</th>
+                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -23,6 +27,14 @@ export default function IngredientsTable(props: { ingredients: Ingredient[] }) {
                                 <td>{ingredient.carbohydrates}</td>
                                 <td>{ingredient.protein}</td>
                                 <td>{ingredient.calories}</td>
+                                <td>
+                                    <button className="btn">
+                                        <FontAwesomeIcon icon={faTrashAlt} />
+                                    </button>
+                                    <button className="btn">
+                                        <FontAwesomeIcon icon={faEdit} />
+                                    </button>
+                                </td>
                             </tr>
                         )
                     })}
