73dccfb32b15f987a2b85152a8615349978b1393
Gherks <nils-joakim.andersson@hotmail.com>
2021-10-04 22:26:25 +0200

    Remove last of the template objects

--- Receptacle.Client/ClientApp/src/App.tsx
+++ Receptacle.Client/ClientApp/src/App.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import { Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
 import IngredientPage from './components/ingredientPage/IngredientPage';
 import RecipeCreationPage from './components/recipeCreationPage/RecipeCreationPage';
 
@@ -14,7 +12,5 @@ export default () => (
         <Route exact path='/' component={Home} />
         <Route path='/ingredients' component={IngredientPage} />
         <Route path='/recipeCreation' component={RecipeCreationPage} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
     </Layout>
 );
