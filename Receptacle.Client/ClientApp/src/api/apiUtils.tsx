6bf722eb17f90ece51799cfceba0df7b9860dd52
Gherks <nils-joakim.andersson@hotmail.com>
2022-01-09 20:10:57 +0100

    Formatting

--- Receptacle.Client/ClientApp/src/api/apiUtils.tsx
+++ Receptacle.Client/ClientApp/src/api/apiUtils.tsx
@@ -1,7 +1,9 @@
 import { toast } from "react-toastify";
 
 export async function handleResponse(response: Response) {
-    if (response.ok) return response.json();
+    if (response.ok) {
+        return response.json();
+    }
     if (response.status === 400) {
         // So, a server-side validation error occurred.
         // Server side validation returns a string error message, so parse as text instead of json.
